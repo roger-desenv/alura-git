cd473ee519b3edd60889a43c4b023ac5d828236e
Roger Costa <roger.desenv@gmail.com>
2023-03-21 07:55:26 -0300

    criando repositório remoto no github

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Botao from '../Botao'
+import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault()
+        console.log('state: ', this.state)
+    }
+
+
     render() {
         return (
-            <form>
-                <div>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
@@ -13,25 +25,32 @@ class Formulario extends React.Component {
                         type="text"
                         name="tarefa"
                         id="tarefa"
-                        placeholder="O que você quer estudar"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value})}
+                        placeholder="O que você quer estudar?"
                         required
                     />
                 </div>
-                <div>
+                <div className={style.inputContainer}>
                     <label>
                         Tempo
                     </label>
-                    <input 
+                    <input
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({ ...this.state, tempo: evento.target.value})}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
                         required
                     />
                 </div>
-                <Botao />
+                <Botao 
+                    texto="Adicionar"
+                />
+                
             </form>
         )
     }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+
+import { useState } from 'react';
+import Item from './Item';
+import style from './Lista.module.scss';
 
 
 function Lista() {
   
-   const tarefas = [{
+   const [tarefas, setTarefas] = useState([{
+
       tarefa: 'React',
       tempo: '02:00:00'
    }, {
       tarefa: 'Javascript',
       tempo: '01:00:00'
-   }]
-    
-  
+      
+   }]);
+
   
    return (
-   <aside>
-      <h2> Estudos do Dia </h2>
+   <aside className={style.listaTarefas}>
+      <h2 onClick={() => {
+         console.log("h2 clicado: ", tarefas)
+         setTarefas ([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00" }])
+      }}> Estudos do Dia </h2>
       <ul>
          {tarefas.map((item, index) => (
-            <li key={index}>
-               <h3>  {item.tarefa}   </h3>
-               <span> {item.tempo} </span>
-            </li>
+            <Item
+               key={index}
+               tarefa={item.tarefa}
+               tempo={item.tempo}
+            />
+
          ))}
 
       </ul>
